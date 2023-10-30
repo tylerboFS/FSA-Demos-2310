b4cb84592d54239dc7f8801aff0c6927f16a5131
Tyler Wright <tyler.wright@fullstackacademy.com>
2023-10-30 09:22:39 -0700

    var to const

--- Unit-2-Javascript/02-Conditional-Statements/02-Activity/Unsolved/script.js
+++ Unit-2-Javascript/02-Conditional-Statements/02-Activity/Unsolved/script.js
@@ -1,6 +1,9 @@
 // Change the values and operators below to test your algorithm meets all conditions
-var x = 50;
-var expression1 = (x < 25);
-var expression2 = (x > 50);
+const x = 50;
+const expression1 = (x < 25);
+const expression2 = (x > 50);
 
 // Write Your JavaScript Code Here
+
+
+
